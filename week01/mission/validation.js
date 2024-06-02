3bfb52e0f188db85b531505cf99341e481ac704c
seunghyeonKang <number42isuser@gmail.com>
2024-06-02 17:10:53 +0900

    [0602] 유효성 검사 js 복습

--- week01/mission/validation.js
+++ week01/mission/validation.js
@@ -1,120 +1,107 @@
-// 제출 완료 모달
-const modalBG = document.querySelector("div.background");
-const modalComponent = document.querySelector("div.modal");
-
-// 유효성 검사
-const formElement = document.querySelector("form#signup_form");
-formElement.addEventListener("submit", validateForm);
-
-// text 초기화
-const nameText = document.querySelector("div.signup p.name");
-const emailText = document.querySelector("div.signup p.email");
-const ageText = document.querySelector("div.signup p.age");
-const passwordText = document.querySelector("div.signup p.password");
-const confirmPasswordText = document.querySelector(
-  "div.signup p.confirm_password"
-);
-
 const passwordPattern =
   /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&.,^#])[A-Za-z\d@$!%*?&.,^#]{4,12}$/;
 
-function setText(element, message, isError) {
-  element.textContent = message;
+// error text 및 모달 변수
+const nameText = document.querySelector("form p.name");
+const emailText = document.querySelector("form p.email");
+const ageText = document.querySelector("form p.age");
+const pwText = document.querySelector("form p.password");
+const confirmPwText = document.querySelector("form p.confirm_password");
+
+const modalBG = document.querySelector("div.background");
+const modalBox = document.querySelector("div.modal");
+
+// 메세지 입력 함수
+function setText(text, textMessage, isError) {
+  text.textContent = textMessage;
   if (isError) {
-    element.classList.add("error");
+    text.classList.add("error");
   } else {
-    element.classList.remove("error");
+    text.classList.remove("error");
   }
 }
 
-function validateForm(event) {
+// 제출 - 유효성 검사 함수
+document.querySelector("form").addEventListener("submit", submitValid);
+
+function submitValid(event) {
   event.preventDefault();
   console.log("제출");
 
-  // input 변수
-  const name = document.querySelector("div.signup input.name").value.trim();
-  const email = document.querySelector("div.signup input.email").value.trim();
-  const age = document.querySelector("div.signup input.age").value.trim();
-  const password = document.querySelector("div.signup input.password").value;
-  const confirmPassword = document.querySelector(
-    "div.signup input.confirm_password"
-  ).value;
-
   let isValid = true;
 
+  // input 변수
+  const nameInput = document.querySelector("form input.name").value.trim();
+  const emailInput = document.querySelector("form input.email").value.trim();
+  const ageInput = document.querySelector("form input.age").value.trim();
+  const pwInput = document.querySelector("form input.password").value;
+  const confirmPwInput = document.querySelector(
+    "form input.confirm_password"
+  ).value;
+
   // 유효성 - 이름
-  if (name === "") {
+  if (nameInput == "") {
     setText(nameText, "필수 입력 항목입니다!", true);
     isValid = false;
   } else {
     setText(nameText, "멋진 이름이네요!", false);
   }
-
   // 유효성 - 이메일
-  if (email === "") {
+  if (emailInput == "") {
     setText(emailText, "필수 입력 항목입니다!", true);
     isValid = false;
-  } else if (!email.includes("@")) {
+  } else if (!emailInput.includes("@")) {
     setText(emailText, "올바른 이메일 형식이 아닙니다.", true);
     isValid = false;
   } else {
     setText(emailText, "올바른 이메일 형식입니다.", false);
   }
-
   // 유효성 - 나이
-  if (age === "") {
+  if (ageInput == "") {
     setText(ageText, "필수 입력 항목입니다!", true);
     isValid = false;
-  } else if (isNaN(age)) {
+  } else if (isNaN(ageInput)) {
     setText(ageText, "숫자로만 입력해주세요.", true);
     isValid = false;
-  } else if (age < 0 || age % 1 !== 0) {
-    setText(ageText, "나이를 입력해주세요.", true);
+  } else if (ageInput < 0 || ageInput % 1 != 0) {
+    setText(ageText, "올바른 나이를 입력해주세요.", true);
     isValid = false;
-  } else if (age < 19) {
+  } else if (ageInput < 19) {
     setText(ageText, "미성년자는 가입할 수 없습니다.", true);
     isValid = false;
   } else {
     setText(ageText, "올바른 나이 형식입니다.", false);
   }
-
   // 유효성 - 비밀번호
-  if (password === "") {
-    setText(passwordText, "필수 입력 항목입니다!", true);
+  if (pwInput == "") {
+    setText(pwText, "필수 입력 항목입니다!", true);
     isValid = false;
-  } else if (!passwordPattern.test(password)) {
-    setText(
-      passwordText,
-      "영어·숫자·특수문자 조합, 4자 이상, 12자리 이하",
-      true
-    );
+  } else if (!passwordPattern.test(pwInput)) {
+    setText(pwText, "영어·숫자·특수문자 조합, 4자 이상, 12자 이하", true);
     isValid = false;
   } else {
-    setText(passwordText, "올바른 비밀번호 형식입니다.", false);
+    setText(pwText, "올바른 비밀번호 형식입니다.", false);
   }
-
   // 유효성 - 비밀번호 확인
-  if (confirmPassword === "" || confirmPassword !== password) {
-    setText(confirmPasswordText, "비밀번호가 일치하지 않습니다.", true);
+  if (confirmPwInput != pwInput) {
+    setText(confirmPwText, "비밀번호가 일치하지 않습니다.", true);
     isValid = false;
   } else {
-    setText(confirmPasswordText, "비밀번호가 일치합니다.", false);
+    setText(confirmPwText, "비밀번호가 일치합니다.", false);
   }
 
-  // 제출
+  // 제출 & 모달
   if (isValid) {
-    showModal();
-    console.log("제출 성공!");
+    openModal();
   }
 }
 
-function showModal() {
+function openModal() {
   modalBG.style.display = "block";
-  modalComponent.style.display = "block";
+  modalBox.style.display = "block";
 }
 
-// 모달 닫기
 document.querySelector("div.modal button").addEventListener("click", () => {
   modalBG.style.display = "none";
-  modalComponent.style.display = "none";
+  modalBox.style.display = "none";
 });
